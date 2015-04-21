b0a0b622066500748001c21b845952d4e22edab5
bob1983 <bob.1983.g@gmail.com>
2015-04-22 02:07:02 +0900

    Added framework

---
+++ SharedFramework/SharedFramework/SharedFramework.h
@@ -0,0 +1,19 @@
+//
+//  SharedFramework.h
+//  SharedFramework
+//
+//  Created by bob on 2015/04/22.
+//  Copyright (c) 2015年 bob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedFramework.
+FOUNDATION_EXPORT double SharedFrameworkVersionNumber;
+
+//! Project version string for SharedFramework.
+FOUNDATION_EXPORT const unsigned char SharedFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedFramework/PublicHeader.h>
+
+
